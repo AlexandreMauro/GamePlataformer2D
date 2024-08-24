79c102e055623d42b8b78402b5394739f1776fde
Alexandre Mauro <94859756+AlexandreMauro@users.noreply.github.com>
2024-08-24 19:05:11 -0300

    Add Spaw Point

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,5 +5,30 @@ using Core.Singelton;
 
 public class GameManager : Singelton<GameManager>
 {
-  
+    [Header("Player")]
+    public GameObject PlayerPrefab;
+
+    [Header("Enemy")]
+    public List<GameObject> EnemysPrefab;
+
+    [Header("Prefences")]
+    public Transform SpawPoint;
+    private GameObject _currentPlayer;
+
+    public void Start()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        SpawPlayer();
+    }
+
+
+    private void SpawPlayer()
+    {
+        _currentPlayer = Instantiate(PlayerPrefab);
+        _currentPlayer.transform.position = SpawPoint.transform.position;
+
+    }
 }
