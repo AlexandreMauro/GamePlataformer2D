9a59794de97bd58920f7a59be228389e82d98bd1
Alexandre Mauro <94859756+AlexandreMauro@users.noreply.github.com>
2024-08-25 19:00:10 -0300

    Add JumpMovement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,21 +5,63 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D PlayerRigibody;
-    public Vector2 Velocity;
-
+    [Header("Friction")]
+    public  Vector2 Friction = new Vector2(.1f,0);
+    [Header("Jump")]
+    public float jumpforce;
+   [Header("Speed")]
     public float Speed;
+    
+    
     private void Update()
     {
-        if(Input.GetKey(KeyCode.D))
+        HandleJump();
+        HandleMovements();
+    }
+
+
+    //HorizontalMovements
+    private void HandleMovements()
+    {
+
+        if (Input.GetKey(KeyCode.D))
         {
             // PlayerRigibody.MovePosition(PlayerRigibody.position + Velocity * Time.deltaTime);
             PlayerRigibody.velocity = new Vector2(Speed, PlayerRigibody.velocity.y);
 
+
         }
-        else if(Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
             PlayerRigibody.velocity = new Vector2(-Speed, PlayerRigibody.velocity.y);
             // PlayerRigibody.MovePosition(PlayerRigibody.position - Velocity * Time.deltaTime);
         }
+
+
+        if (PlayerRigibody.velocity.x < 0)
+        {
+            PlayerRigibody.velocity += Friction;
+
+
+        }
+        else if (PlayerRigibody.velocity.x > 0)
+        {
+            PlayerRigibody.velocity -= Friction;
+           
+        }
+       
+
+
+    }
+
+    //Jump Code
+    private void HandleJump()
+    {
+       if(Input.GetKeyDown(KeyCode.W))
+        {
+            PlayerRigibody.velocity = Vector2.up * jumpforce;
+
+        }
     }
 }
+
