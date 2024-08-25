41794059d8c5c5ea19d1090d64bd0c9196ef21a8
Alexandre Mauro <94859756+AlexandreMauro@users.noreply.github.com>
2024-08-25 20:11:51 -0300

    Run Feature

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     public Rigidbody2D PlayerRigibody;
     [Header("Friction")]
     public  Vector2 Friction = new Vector2(.1f,0);
-   
+    private Vector2 StopFriction = new Vector2(.1f,0);
     
     [Header("Jump")]
     public float jumpforce;
@@ -28,58 +28,44 @@ public class Player : MonoBehaviour
     private void HandleMovements()
     {
 
-
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            _currentSpeed = RunSpeed;
+        }
+        else
+        {
+            _currentSpeed = Speed;
+        }
 
 
         if (Input.GetKey(KeyCode.D))
         {
             // PlayerRigibody.MovePosition(PlayerRigibody.position + Velocity * Time.deltaTime);
-            PlayerRigibody.velocity = new Vector2(Speed, PlayerRigibody.velocity.y);
+            PlayerRigibody.velocity = new Vector2(_currentSpeed, PlayerRigibody.velocity.y);
 
 
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            PlayerRigibody.velocity = new Vector2(-Speed, PlayerRigibody.velocity.y);
+            PlayerRigibody.velocity = new Vector2(-_currentSpeed, PlayerRigibody.velocity.y);
             // PlayerRigibody.MovePosition(PlayerRigibody.position - Velocity * Time.deltaTime);
         }
 
 
 
-        if(PlayerRigibody.velocity.x != 0)
-        {
+     
 
             if (PlayerRigibody.velocity.x > 0)
             {
-                PlayerRigibody.velocity -= Friction;
+                PlayerRigibody.velocity -= Friction ;
 
 
             }
             else if (PlayerRigibody.velocity.x < 0)
             {
-                PlayerRigibody.velocity += Friction;
-
-
-            }
-        }else if (PlayerRigibody.velocity.x == 0)
-        {
-            if (Friction.x != 1f)
-            {
-                Friction = new Vector2(.1f + .2f, 0);
-            }
-            else
-            {
-                Friction = new Vector2(1f, 0);
-            }
-
-
+            PlayerRigibody.velocity += Friction;
         }
       
-       
-            
-        
-
-
     }
 
     //Jump Code
